d1a1e7325bc82775ca5e96f91d95189968660e00
sudeakq <sudeakq@gmail.com>
2024-03-16 16:35:01 +0300

    first app

---
+++ first_app.py
@@ -0,0 +1,18 @@
+
+ad="sude"
+soyad="akgun"
+musteriad= ad +" "+ soyad
+cinsiyet="kadin"
+tc=2873129
+dogumyili=2003
+adress="kfjjsd djfslkj flkdjgdkj"
+yasi=21
+
+print(ad)
+print(soyad)
+print(musteriad)
+print(cinsiyet)
+print(tc)
+print(dogumyili)
+print(adress)
+print(yasi)
---
+++ type-conversion.py
@@ -0,0 +1,6 @@
+x= input('1.sayı: ')
+y= input('2.sayı: ')
+
+print(type(x))
+
+toplam=int(x)+int(y)
